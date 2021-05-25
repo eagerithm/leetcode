6044282c93cf71c34f93886ae89ca624e750d15c
chobobdev <chobobdev@naver.com>
2021-05-25 21:55:11 +0900

    new version

--- chobobdev/27/solution.go
+++ chobobdev/27/solution.go
@@ -1,14 +1,9 @@
 func removeElement(nums []int, val int) int {
-	var temp []int
-	for _, num := range nums {
-		if num != val {
-			temp = append(temp, num)
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+			i--
 		}
 	}
-	//It doesn't matter what you leave beyond the returned length. For example if you return 2 with nums = [2,2,3,3] or nums = [2,2,0,0], your answer will be accepted.
-	//위 전재조건을 만족시키기 위해 temp를 copy 해서 앞 원소들을 바꾸어 준다
-	copy(nums, temp)
-
-	return len(temp)
-
+	return len(nums)
 }
