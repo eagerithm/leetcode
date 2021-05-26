4c031f055a08aa3dea225c41b3646bdf11bd0b85
chobobdev <chobobdev@naver.com>
2021-05-27 00:04:24 +0900

    [ 버니-210527] - 문제 26 완료

---
+++ chobobdev/26/solution.go
@@ -0,0 +1,10 @@
+func removeDuplicates(nums []int) int {
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			nums = append(nums[:i], nums[i+1:]...)
+			i--
+		}
+	}
+	return len(nums)
+
+}
