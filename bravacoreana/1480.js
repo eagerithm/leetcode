9e587702caaa76e031330fe994b204f72e9a3a83
Liha <bravacoreana@gmail.com>
2021-05-26 10:35:10 +0200

    [liha-210526] - #1431

--- bravacoreana/1480.js
+++ bravacoreana/1480.js
@@ -1,21 +1,21 @@
-// < 1480. Running Sum of 1d Array >
+// << 1480. Running Sum of 1d Array >>
 
 /*
 Given an array nums. 
 We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 Return the running sum of nums.
 
-Example 1:
+- Example 1:
 Input: nums = [1,2,3,4]
 Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 
-Example 2:
+- Example 2:
 Input: nums = [1,1,1,1,1]
 Output: [1,2,3,4,5]
 Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
 
-Example 3:
+- Example 3:
 Input: nums = [3,1,2,10,1]
 Output: [3,4,6,16,17]
 
@@ -24,7 +24,7 @@ Constraints:
 -10^6 <= nums[i] <= 10^6
 */
 
-// ===== Start from here: ===== //
+// ========== START FROM HERE: ========== //
 
 /**
  * @param {number[]} nums
@@ -36,32 +36,42 @@ const nums1 = [1, 2, 3, 4];
 const nums2 = [1, 1, 1, 1, 1];
 const nums3 = [3, 1, 2, 10, 1];
 
+// ========== START FROM HERE: ========== //
+
 // - way 1
-// var runningSum = function (nums) {
-//   let total = 0;
-//   let newArray = [];
-//   for (a of nums) {
-//     total += a;
-//     newArray.push(total);
-//   }
-//   return newArray;
-// };
+var runningSum1 = function (nums) {
+  let total = 0;
+  let newArray = [];
+  for (a of nums) {
+    total += a;
+    newArray.push(total);
+  }
+  return newArray;
+};
+
+runningSum1(nums1);
+runningSum1(nums2);
+runningSum1(nums3);
 
 // - way 2
 /*
 Runtime: 76 ms, faster than 88.53% of JavaScript online submissions for Running Sum of 1d Array.
 Memory Usage: 38.7 MB, less than 84.38% of JavaScript online submissions for Running Sum of 1d Array.
 */
-// var runningSum = function (nums) {
-//   let total = 0;
-//   let newArray = [];
-//   for (let i = 0; i < nums.length; i++) {
-//     total += nums[i];
-//     newArray.push(total);
-//   }
+var runningSum2 = function (nums) {
+  let total = 0;
+  let newArray = [];
+  for (let i = 0; i < nums.length; i++) {
+    total += nums[i];
+    newArray.push(total);
+  }
 
-//   return newArray;
-// };
+  return newArray;
+};
+
+runningSum2(nums1);
+runningSum2(nums2);
+runningSum2(nums3);
 
 // - way 3
 
@@ -70,13 +80,13 @@ Runtime: 80 ms, faster than 69.63% of JavaScript online submissions for Running 
 Memory Usage: 39 MB, less than 47.69% of JavaScript online submissions for Running Sum of 1d Array.
 */
 
-var runningSum = function (nums) {
+var runningSum3 = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     i === 0 ? (nums[i] = nums[i]) : (nums[i] = nums[i - 1] + nums[i]);
     console.log(nums);
   }
 };
 
-runningSum(nums1);
-runningSum(nums2);
-runningSum(nums3);
+runningSum3(nums1);
+runningSum3(nums2);
+runningSum3(nums3);
