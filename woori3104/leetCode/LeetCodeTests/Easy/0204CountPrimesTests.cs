b5392877bebc802b1227e470c6047ec706edd641
woori3104 <sizuku11261@gmail.com>
2021-05-30 22:00:31 +0900

    #1480, #1672, #204

---
+++ woori3104/leetCode/LeetCodeTests/Easy/0204CountPrimesTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy.Tests
+{
+    [TestClass()]
+    public class CountPrimesTests
+    {
+        [TestMethod()]
+        public void CountPrimesTest()
+        {
+            var c = new CountPrimesClass();
+
+            // example1
+            var target = 10;
+            var result = c.CountPrimes(target);
+            var expected = 4;
+            Assert.AreEqual(expected, result);
+
+            // example2
+            target = 0;
+            result = c.CountPrimes(target);
+            expected = 0;
+            Assert.AreEqual(expected, result);
+
+            //example3
+            target = 1;
+            result = c.CountPrimes(target);
+            expected = 0;
+            Assert.AreEqual(expected, result);
+
+            //example4
+            target = 2;
+            result = c.CountPrimes(target);
+            expected = 0;
+            Assert.AreEqual(expected, result);
+
+            //example5
+            target = 3;
+            result = c.CountPrimes(target);
+            expected = 1;
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ woori3104/leetCode/leetCode/Easy/0204CountPrimes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    public class CountPrimesClass
+    {
+        /*
+         **    Count the number of prime numbers less than a non-negative number, `n`.
+         **
+         **   **Example 1:**
+         **
+         **   ```
+         **   Input: n = 10
+         **   Output: 4
+         **   Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+         **
+         **   ```
+         **
+         **   **Example 2:**
+         **
+         **   ```
+         **   Input: n = 0
+         **   Output: 0
+         **
+         **   ```
+         **
+         **   **Example 3:**
+         **
+         **   ```
+         **   Input: n = 1
+         **   Output: 0
+         **
+         **   ```
+         **
+         **   **Constraints:**
+         **
+         **   - `0 <= n <= 5 * 106`
+          */
+
+        // Way1 : Time Limit
+        public int CountPrimes(int n)
+        {
+            if (n < 3) return 0;
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            for (var i = 2; i < n; i++)
+            {
+                for (var j = 0; j < primes.Count; j++)
+                {
+                    if (i % primes[j] == 0)
+                        break;
+                    if (j + 1 % primes[j] == primes.Count)
+                    {
+                        primes.Add(i);
+                    }
+                }
+            }
+            return primes.Count;
+        }
+    }
+}
