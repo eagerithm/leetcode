708b7339cd1254ba92bc074fed4b7a9260532e34
supericon <supericon@supericonui-MacBookPro.local>
2021-06-02 15:03:56 +0900

    Two Sum 피드백 적용

--- JeongSeongHun/Two Sum/index.js
+++ JeongSeongHun/Two Sum/index.js
@@ -3,13 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    for(let i = 0; i<nums.length-1; i++){
-        for(let j = i+1; j<nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return[i,j];
-                break;
-            }
-        }
+const twoSum = function (nums, target) {
+  const comp = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] !== undefined) {
+      return [comp[nums[i]], i];
     }
-}
+    comp[target - nums[i]] = i;
+  }
+};
