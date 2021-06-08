9f78e6f319286d05ae40443a51eec8f2d9aec6ca
chobobdev <chobobdev@naver.com>
2021-06-08 23:17:39 +0900

    20210608-chobobdev-restructuring repo

---
+++ chobobdev/69/69.go
@@ -0,0 +1,6 @@
+func mySqrt(x int) int {
+	return int(math.Sqrt(float64(x)))
+}
+
+// Runtime: 4 ms, faster than 53.66% of Go online submissions for Sqrt(x).
+// Memory Usage: 2.4 MB, less than 100.00% of Go online submissions for Sqrt(x).
