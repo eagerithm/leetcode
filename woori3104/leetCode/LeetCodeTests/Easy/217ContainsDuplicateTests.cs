cdab2df243772f09a94b4aaa88540e926ffef67e
woori3104 <sizuku11261@gmail.com>
2021-07-15 22:33:07 +0900

    【woori-210601】28, 217 풀이 추가 (#30)
    * 28, 29 풀이추가
    
    * substring 사용하도록 수정

---
+++ woori3104/leetCode/LeetCodeTests/Easy/217ContainsDuplicateTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy.Tests
+{
+    [TestClass()]
+    public class ContainsDuplicateTests
+    {
+        [TestMethod()]
+        public void ContainsDuplicateTest()
+        {
+            // Example1
+            var c = new ContainsDuplicateClass();
+            int[] nums = { 1, 2, 3, 1 };
+            var result = c.ContainsDuplicate(nums);
+            Assert.IsTrue(result);
+
+            // Example2
+            int[] nums2 = { 1, 2, 3, 4 };
+            result = c.ContainsDuplicate(nums2);
+            Assert.IsFalse(result);
+
+            // Example3
+            int[] nums3 = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+            result = c.ContainsDuplicate(nums3);
+            Assert.IsTrue(result);
+
+
+        }
+    }
+}
---
+++ woori3104/leetCode/leetCode/Easy/217ContainsDuplicate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    public class ContainsDuplicateClass
+    {
+        /**
+         ** Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+         **
+         **
+         **
+         ** Example 1:
+         **
+         ** Input: nums = [1,2,3,1]
+         ** Output: true
+         ** Example 2:
+         **
+         ** Input: nums = [1,2,3,4]
+         ** Output: false
+         ** Example 3:
+         **
+         ** Input: nums = [1,1,1,3,3,4,3,2,4,2]
+         ** Output: true
+         **
+         **
+         ** Constraints:
+         **
+         ** 1 <= nums.length <= 105
+         ** -109 <= nums[i] <= 109
+         **/
+
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var dic = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    return true;
+                dic.Add(nums[i], i);
+            }
+            return false;
+        }
+    }
+}
