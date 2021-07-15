2f49e51377a73310a05462b630ac5886045a1aa7
woori3104 <sizuku11261@gmail.com>
2021-07-15 22:44:00 +0900

    035, 205 풀이 추가 (#28)

---
+++ woori3104/leetCode/leetCode/Easy/035SearchInsertPosition.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    public class SearchInsertPosition
+    {
+        /**
+         **   Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+         **
+         **   You must write an algorithm with O(log n) runtime complexity.
+         **
+         **
+         **
+         **   Example 1:
+         **
+         **   Input: nums = [1,3,5,6], target = 5
+         **   Output: 2
+         **   Example 2:
+         **
+         **   Input: nums = [1,3,5,6], target = 2
+         **   Output: 1
+         **   Example 3:
+         **
+         **   Input: nums = [1,3,5,6], target = 7
+         **   Output: 4
+         **   Example 4:
+         **
+         **   Input: nums = [1,3,5,6], target = 0
+         **   Output: 0
+         **   Example 5:
+         **
+         **   Input: nums = [1], target = 0
+         **   Output: 0
+         **
+         **
+         **   Constraints:
+         **
+         **   1 <= nums.length <= 104
+         **   -104 <= nums[i] <= 104
+         **   nums contains distinct values sorted in ascending order.
+         **   -104 <= target <= 104
+         **/
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= target)
+                    return i;
+            }
+            return nums.Length;
+        }
+    }
+}
