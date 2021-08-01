3e3269e64bbf7532a01ce95f57e2033278c9cb0f
woori3104 <sizuku11261@gmail.com>
2021-08-01 21:32:09 +0900

    Modify : README

--- woori3104/leetCode/leetCode/Easy/066PlusOne.cs
+++ woori3104/leetCode/leetCode/Easy/066PlusOne.cs
@@ -4,6 +4,40 @@ using System.Linq;
 
 public class PlusOneClass
 {
+    /*
+    * Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+    *
+    *   The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+    *
+    *   You may assume the integer does not contain any leading zero, except the number 0 itself.
+    *
+    *
+    *
+    *   Example 1:
+    *
+    *   Input: digits = [1,2,3]
+    *   Output: [1,2,4]
+    *   Explanation: The array represents the integer 123.
+    *   Example 2:
+    *
+    *   Input: digits = [4,3,2,1]
+    *   Output: [4,3,2,2]
+    *   Explanation: The array represents the integer 4321.
+    *   Example 3:
+    *
+    *   Input: digits = [0]
+    *   Output: [1]
+    *
+    *
+    *   Constraints:
+    *
+    *   1 <= digits.length <= 100
+    *   0 <= digits[i] <= 9
+    *
+    */
+
+    // Runtime: 232 ms, faster than 81.67% of C# online submissions for Plus One.
+    // Memory Usage: 30.8 MB, less than 45.02% of C# online submissions for Plus One.
     public int[] PlusOne(int[] digits)
     {
         // 배열을 List로 변환 (데이터를 추가하게 되는 경우를 위해)
