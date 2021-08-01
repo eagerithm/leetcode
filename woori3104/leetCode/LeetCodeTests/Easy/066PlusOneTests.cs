826613d379fbcebae5bcc334b324236a06c166ba
woori3104 <sizuku11261@gmail.com>
2021-08-01 21:28:54 +0900

    ADD : plusOne

---
+++ woori3104/leetCode/LeetCodeTests/Easy/066PlusOneTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Easy
+{
+    [TestClass()]
+    public class PlusOneTests
+    {
+        [TestMethod()]
+        public void PlusOneTest()
+        {
+            var c = new PlusOneClass();
+
+            // example1
+            int[] input = { 9 };
+            var result = c.PlusOne(input);
+            int[] expected = { 1, 0 };
+            for (int i = 0; i < result.Length; i++) {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+
+            // example2
+            int[] input1 = { 9, 9 };
+            var result2 = c.PlusOne(input1);
+            int[] expected2 = { 1, 0, 0 };
+            for (int i = 0; i < result2.Length; i++)
+            {
+                Assert.AreEqual(expected2[i], result2[i]);
+            }
+
+            // example3
+            int[] input3 = { 1, 2 };
+            var result3 = c.PlusOne(input3);
+            int[] expected3 = { 1, 3};
+            for(int i = 0; i < result3.Length; i++) {
+                Assert.AreEqual(expected3[i], result3[i]);
+            }
+        }
+    }
+}
---
+++ woori3104/leetCode/leetCode/Easy/066PlusOne.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+public class PlusOneClass
+{
+    public int[] PlusOne(int[] digits)
+    {
+        // 배열을 List로 변환 (데이터를 추가하게 되는 경우를 위해)
+        List<int> results = digits.ToList();
+
+        // 기존 마지막 인덱스를 취득
+        int last = digits.Length - 1;
+        // 마지막 값에 1을 더함
+        results[last] = results[last] + 1;
+
+        // 마지막 값이 10을 넘는 경우 
+        while (results[last] > 9)
+        {
+            // 10이 넘는 값이 마지막 값이 아닌경우
+            if (last > 0 && results[last] > 9)
+            {
+                results[last] = results[last] - 10;
+                results[last - 1] = results[last - 1] + 1;
+            }
+            // 10이 넘는 경우 값이 마지막 값인 경우
+            else if (results[last] > 9)
+            {
+                results[last] = results[last] - 10;
+                // 리스트의 첫번째의 1을 추가해줌
+                results.Insert(0, 1);
+                last++;
+            }
+            last--;
+        }
+        return results.ToArray();
+    }
+}
